2639797e22eac27835d51783947ef84e3089cc0b
Arjun Ganesan <me@arjun-g.com>
2020-05-10 23:51:34 +0530

    Updates

---
+++ precache-manifest.eef75f97abcb40517b78b6bd22a2d744.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc05206149654aef2a509d3a9e25d90c",
+    "url": "/speech-markdown-editor/index.html"
+  },
+  {
+    "revision": "19a3375e479cabf7e0e2",
+    "url": "/speech-markdown-editor/static/css/2.e5ed3f04.chunk.css"
+  },
+  {
+    "revision": "1ec5eaa482e08db02628",
+    "url": "/speech-markdown-editor/static/css/main.79ad1d4a.chunk.css"
+  },
+  {
+    "revision": "19a3375e479cabf7e0e2",
+    "url": "/speech-markdown-editor/static/js/2.90d71711.chunk.js"
+  },
+  {
+    "revision": "247c6e543d47263473a21e58c128bdd7",
+    "url": "/speech-markdown-editor/static/js/2.90d71711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec5eaa482e08db02628",
+    "url": "/speech-markdown-editor/static/js/main.bf985b2b.chunk.js"
+  },
+  {
+    "revision": "09fcdea1da5e8fc77be0",
+    "url": "/speech-markdown-editor/static/js/runtime-main.f87a4678.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/speech-markdown-editor/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/speech-markdown-editor/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/speech-markdown-editor/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/speech-markdown-editor/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/speech-markdown-editor/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/speech-markdown-editor/static/media/icons-20.ca7934b3.eot"
+  }
+]);
